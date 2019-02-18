82d425b03cd3da6c200f0f8e4844d25bf79d77d1
mohamed nabil <mohamedn085@gmail.com>
2019-02-18 19:05:07 +0200

    third commit for relationship

--- app/Model/Review.php
+++ app/Model/Review.php
@@ -2,9 +2,20 @@
 
 namespace App\Model;
 
+use App\Model\Product;
+
 use Illuminate\Database\Eloquent\Model;
 
 class Review extends Model
 {
     //
+
+
+
+    public function products()
+    {
+    
+       return $this->belongsTo(Product::class);
+    
+    }
 }
--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -1,9 +1,16 @@
 <?php
 
+
 use Faker\Generator as Faker;
 
+
 $factory->define(App\Model\Product::class, function (Faker $faker) {
-    return [
-        //
+    return
+    [
+      'name'   =>$faker->word,
+      'details'=>$faker->paragraph,
+      'price'  =>$faker->numberBetween(100,1000),
+      'stock'  =>$faker->randomDigit,
+      'discount'=>$faker->numberBetween(2,30)
     ];
 });
